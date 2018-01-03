836af578b7a2ab4c77f53ae75b79c9fc0f9ab7b3
Murilo Cesar Camargo <mu_riba@hotmail.com>
2018-01-02 23:05:53 -0200

    02012018

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IEmailService _emailService;
+
+        public AppController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +21,13 @@ namespace TheWorld.Controllers.Web
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel contactViewModel)
+        {
+            _emailService.SendEmail("murilo.ccamargo@gmail.com", contactViewModel.Email, "From TheWorld", contactViewModel.Message);
             return View();
         }
 
---
+++ TheWorld/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace TheWorld.Services
+{
+    public interface IEmailService
+    {
+        void SendEmail(string to, string from, string subject, string body);
+    }
+}
